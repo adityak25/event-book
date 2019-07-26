17f6bbbc68bab77c2e5b5a6c9ddd8a96312a8fff
adityak25 <adityak25@users.noreply.github.com>
2019-07-26 01:46:24 -0700

    setup with graphql and mongodb

--- app.js
+++ app.js
@@ -1,11 +1,74 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
-app.get('/', (req, res, nex) => {
-    res.send('Hello Aditya!!!');
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return events;
+        },
+        createEvent: (args) => {
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: args.eventInput.price,
+                date: args.eventInput.date.toString()
+            };
+            events.push(event);
+            return event;
+        }
+    },
+    graphiql: true
+}));
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}
+@cluster0-d3mtl.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect( () => {
+        app.listen(3000);
+    }
+    ,err => {
+  //const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log(err);
+  client.close();
 });
-app.listen(3000);
