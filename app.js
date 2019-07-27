ca2014006b96df372527d5a80269e6588fcd56e6
Aditya  Kankanala <adityak25@users.noreply.github.com>
2019-07-26 17:54:43 -0700

    added events model

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
-const mongoose = require("mongoose");
-const MongoClient = require("mongodb").MongoClient;
+const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+
+const Event = require('./models/event');
 
 const app = express();
 
-const events = [];
-
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
+app.use(
+  '/graphql',
+  graphqlHttp({
     schema: buildSchema(`
         type Event {
             _id: ID!
@@ -42,33 +44,52 @@ app.use('/graphql', graphqlHttp({
         }
     `),
     rootValue: {
-        events: () => {
-            return events;
-        },
-        createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: args.eventInput.price,
-                date: args.eventInput.date.toString()
-            };
-            events.push(event);
-            return event;
-        }
+      events: () => {
+        return events;
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      }
     },
     graphiql: true
-}));
+  })
+);
 
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${
+  process.env.MONGO_PASSWORD
+}
 @cluster0-d3mtl.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect( () => {
-        app.listen(3000);
-    }
-    ,err => {
-  //const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log(err);
-  client.close();
-});
+
+client.connect().then(
+  () => {
+    app.listen(3000);
+  },
+  err => {
+    console.log(err);
+    // perform actions on the collection object
+    client.close();
+  }
+);
+
+// mongoose
+//   .connect(
+//     'mongodb+srv://aditya:l4eV9kOiE2J71oBu@cluster0-d3mtl.gcp.mongodb.net/',
+//     { dbName: 'test' }
+//   )
